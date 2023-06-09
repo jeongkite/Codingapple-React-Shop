042b2b77b14986831323ea7b199311d9457bc76f
Jeongyeon Yang <jeong6kite@gmail.com>
2023-06-09 16:01:35 +0900

    내가 만든 페이지 라우팅

--- shop/src/App.js
+++ shop/src/App.js
@@ -4,11 +4,10 @@ import { useState } from 'react';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap'
 import { Routes, Route, Link } from 'react-router-dom'
 
-import data from './data.js'
+import MainPage from './Components/Main';
+import DetailPage from './Components/Detail';
 
 function App() {
-
-  let [items] = useState(data)
 
   return (
     <div className="App">
@@ -25,43 +24,14 @@ function App() {
       </Navbar>
 
       <Routes>
-        <Route path='/' element={
-          <div>
-            <div className='main-bg'></div>
-            <br />
-            <div className="container">
-              <div className="row">
-                {
-                  items.map(function (item, i) {
-                    return (
-                      <Card item={item} />
-                    )
-                  })
-                }
-              </div>
-            </div>
-          </div>
-        } />
-        <Route path='/detail' element={<div>상세페이지다,,~ 이마리야!!</div>} />
+        <Route path='/' element={ <MainPage /> } />
+        <Route path='/detail' element={ <DetailPage /> } />
         <Route path='/about' element={<div>어바웃입니다 여기는 쿠쿠루삥뽕빵</div>} />
       </Routes>
 
-      <br />
-
-      <Button variant="primary">Primary</Button>
     </div>
   );
 }
 
-function Card(props) {
-  return (
-    <div className="col-md-4">
-      <img src={'/present' + (props.item.id + 1) + '.jpeg'} width="80%" />
-      <h4>{props.item.title}</h4>
-      <p>{props.item.content}</p>
-      <p>{props.item.price}</p>
-    </div>
-  )
-}
 
 export default App;
---
+++ shop/src/Components/Main.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import data from './../data.js'
+import { Link } from 'react-router-dom'
+
+function MainPage() {
+    let [items] = useState(data)
+
+    return (
+        <div>
+            <div className='main-bg'></div>
+            <br />
+            <div className="container">
+                <div className="row">
+                    {
+                        items.map(function (item, i) {
+                            return (
+                                <Link to={"/detail"}>
+                                    <Card item={item} />
+                                </Link>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Card(props) {
+    return (
+        <div className="col-md-4">
+            <img src={'/present' + (props.item.id + 1) + '.jpeg'} width="80%" />
+            <h4>{props.item.title}</h4>
+            <p>{props.item.content}</p>
+            <p>{props.item.price}</p>
+        </div>
+    )
+}
+
+export default MainPage;
