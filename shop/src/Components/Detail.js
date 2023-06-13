5a8e2fc267937894635558958a1ad1e1ee074bb2
Jeongyeon Yang <jeong6kite@gmail.com>
2023-06-13 18:48:07 +0900

    Lifecycle과 useEffect 2

--- shop/src/Components/Detail.js
+++ shop/src/Components/Detail.js
@@ -1,10 +1,11 @@
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { useEffect, useState } from "react";
+import Form from 'react-bootstrap/Form';
 
 let Btn = styled.button`
-    background: ${ props => props.bg };
-    color: ${ props => props.bg == 'blue' ? 'white' : 'black' };
+    background: ${props => props.bg};
+    color: ${props => props.bg == 'blue' ? 'white' : 'black'};
     padding: 10px;
     border: none;
 `
@@ -21,26 +22,35 @@ let YellowBox = styled.div`
 `
 
 function DetailPage(props) {
-    let {itemId} = useParams();
-    let item = props.items.find((item)=> item.id == itemId )
+    let { itemId } = useParams();
+    let item = props.items.find((item) => item.id == itemId)
 
     let [isBosHidden, setIsBoxHidden] = useState(false)
+    let [count, setCount] = useState("")
+    let [isAlertHidden, setIsAlertHidden] = useState(true)
 
     useEffect(() => {
-        setTimeout(() => setIsBoxHidden(true), 2000 )
-    });
+        let timer = setTimeout(() => setIsBoxHidden(true), 2000);
+        isNaN(count) ? setIsAlertHidden(false) : setIsAlertHidden(true)
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [count]);
 
     return (
         <div className="container">
-            { !isBosHidden && <YellowBox></YellowBox> }
+            {!isBosHidden && <div className="alert alert-warning">2초 이내 구매시 할인!</div>}
             <div className="row">
                 <div className="col-md-6">
                     <img src={"https://codingapple1.github.io/shop/shoes" + (item.id + 1) + ".jpg"} width="100%" />
                 </div>
                 <div className="col-md-6">
-                    <h4 className="pt-5">{ item.title }</h4>
-                    <p>{ item.content }</p>
-                    <p>{ item.price }</p>
+                    <h4 className="pt-5">{item.title}</h4>
+                    <p>{item.content}</p>
+                    <p>{item.price}</p>
+                    { !isAlertHidden && <div className="alert alert-danger">숫자만 입력해주세요!</div> }
+                    <input onChange={ (obj) => { setCount(obj.target.value) }} type="text" className="form-control"/>
+                    <br />
                     <button className="btn btn-danger">주문하기</button>
                 </div>
             </div>
